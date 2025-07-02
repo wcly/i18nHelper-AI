46f844b642a3799bef1ab2eb24ad0ef34c4b5e15
GitHub Action <action@github.com>
2025-07-02 06:05:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/wcly/i18nhelperai/MyBundle.kt
+++ src/main/kotlin/com/github/wcly/i18nhelperai/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wcly.i18nhelperai
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wcly/i18nhelperai/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/wcly/i18nhelperai/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.wcly.i18nhelperai.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
